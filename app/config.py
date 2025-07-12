7c9651b3a1f423fd5604571b0e1597c4917c9d5e
ishan <ishanlenin2006@gmail.com>
2025-07-13 00:05:52 +0530

    Add Procfile for Heroku deployment

--- app/config.py
+++ app/config.py
@@ -1,16 +1,26 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from app.config import settings  # Import settings from config module
 
 
 class Settings(BaseSettings):
+    
+
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if SQLALCHEMY_DATABASE_URL is None:
+    # fallback to local settings if DATABASE_URL not found (for local development)
+         SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
     
     database_hostname: str = "localhost"
     database_port: str = "5432"
     database_password: str = "CHACHU2206"
     database_name: str = "fastapi"
     database_username: str = "postgres"
-    secret_key: str = "mysecretkey"  # Example secret key for JWT
+    secret_key: str = "secretkey1010101"  # Example secret key for JWT
     algorithm: str = "HS256"  # Example algorithm for JWT
-    access_token_expire_minutes: int = 30  # Token expiration time in minutes
+    access_token_expire_minutes: int = 60  # Token expiration time in minutes
 
     model_config = SettingsConfigDict(env_file=".env")
 
